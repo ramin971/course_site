fb4fec590f95d4dbd44d693bb8fbeec1ade688d3
ramin <ramin.rahmani971@gmail.com>
2021-12-22 08:57:22 +0330

    meetup model and meetup detail page added ,config to show upload picture

--- course_site/meetups/admin.py
+++ course_site/meetups/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Meetup
 # Register your models here.
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ['title',]
+    # list_filter = ['']
+    prepopulated_fields = {'slug':('title',)}
+    search_fields = ['title','description']
+
+
+admin.site.register(Meetup,MeetupAdmin)
--- course_site/meetups/views.py
+++ course_site/meetups/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from .models import Meetup
 # Create your views here.
+
+def index(request):
+    all_obj=Meetup.objects.all()
+
+
+    return render(request,'meetups/index.html',{
+        'meetups': all_obj,
+
+    })
+def meetup_details(request,slug):
+    obj=get_object_or_404(Meetup,slug=slug)
+    return render(request,'meetups/meetup-detail.html',{
+        'meetup':obj,
+    })
