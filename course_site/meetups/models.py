3eace34303390fd8b17388c5cd5c5f2bd8925f74
ramin <ramin.rahmani971@gmail.com>
2021-12-24 22:12:06 +0330

    add participant model and date field to meetup model

--- course_site/meetups/models.py
+++ course_site/meetups/models.py
@@ -8,11 +8,23 @@ class Location(models.Model):
     def __str__(self):
         return '{},({})'.format(self.name,self.address)
 
+class Participant(models.Model):
+    email=models.EmailField(unique=True)
+    def __str__(self):
+        return self.email
+
 class Meetup(models.Model):
     title=models.CharField(max_length=200)
     slug=models.SlugField(unique=True)
+    date=models.DateField()
+    organizer_email=models.EmailField()
     description=models.TextField()
     image=models.ImageField(upload_to='image')
     location=models.ForeignKey(Location,on_delete=models.CASCADE,related_name='meetups')
+    participant=models.ManyToManyField(Participant,blank=True,null=True,related_name='meetups')
+
+    def get_participant(self):
+        return ",\n".join([str(p) for p in self.participant.all()])
+
     def __str__(self):
         return self.title
