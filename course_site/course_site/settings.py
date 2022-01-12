99ad61783160a5ff8535ffc70b92edd509242fe9
ramin <ramin.rahmani971@gmail.com>
2022-01-12 12:15:54 +0330

    final

--- course_site/course_site/settings.py
+++ course_site/course_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&bo4n@&i57qt&z(!bz2u891reyh89f+#^#)er0$dfw9(dual8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
